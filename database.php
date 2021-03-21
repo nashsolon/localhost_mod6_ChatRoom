8fad93e6126321b4c7939a54c126982c54f12d2d
sashachack <schackalackal@wustl.edu>
2021-03-20 19:12:21 -0500

    stuff

--- database.php
+++ database.php
@@ -1,3 +1,7 @@
+<!-- Note from Sasha, copy pasted from FAQ -->
+<!-- Should I use a database for Module 6?
+You can store everything locally in this module. No need to use a database. -->
+
 <?php
     // Content of database.php
     // This connects the database to our php
