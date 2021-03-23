d0a2472ab1695c94d2a4f826eaa8b1183e60810b
sashachack <schackalackal@wustl.edu>
2021-03-23 12:40:15 -0500

    styff

--- chat-server.js
+++ chat-server.js
@@ -1,4 +1,3 @@
-
 // Require the packages we will use:
 const http = require("http"),
     fs = require("fs");
@@ -6,10 +5,10 @@ const http = require("http"),
 const port = 3456;
 const file = "client.html";
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html, on port 3456:
-const server = http.createServer(function (req, res) {
+const server = http.createServer(function(req, res) {
     // This callback runs when a new connection is made to our HTTP server.
 
-    fs.readFile(file, function (err, data) {
+    fs.readFile(file, function(err, data) {
         // This callback runs when the client.html file has been read from the filesystem.
 
         if (err) return res.writeHead(500);
@@ -21,7 +20,7 @@ server.listen(port);
 
 // Import Socket.IO and pass our HTTP server object to it.
 const socketio = require("socket.io")(server, {
-   
+
 });
 
 // Attach our Socket.IO server to our HTTP server to listen
@@ -29,27 +28,18 @@ const io = socketio.listen(server);
 
 //Server side creating a new room
 
-    
 
 
-io.sockets.on("connection", function (socket) {
+
+io.sockets.on("connection", function(socket) {
     socket.on('create', function(room) {
-        
-        socket.join(room["room_name"]);
-        
-        console.log("The room you joined is: " + room["room_name"]);
-        io.sockets.emit("add_to_roomlist", { room_name: room["room_name"] })
-    });
-
-    socket.on('message_to_server_room', function(room) {
-        console.log("The room you joined is" + room["room_name"]);
-        socket.join(room["room_name"]);
-        io.to(room["room_name"]).emit("message_to_room", {message: room["message"] });
+        socket.join(room);
+        io.sockets.emit("create_room", { message: room["message"] })
         console.log("New room name: " + room["room_name"]);
     });
     // This callback runs when a new Socket.IO connection is established.
 
-    socket.on('message_to_server', function (data) {
+    socket.on('message_to_server', function(data) {
         // This callback runs when the server receives a new message from the client.
 
         console.log("message: " + data["message"]); // log it to the Node.JS output
